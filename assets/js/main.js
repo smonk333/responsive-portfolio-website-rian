44de627e6402cf32c1c3aa2c0e8c84a14dcde908
smoke <gboling829@gmail.com>
2025-03-26 19:14:59 -0400

    fixed scroll active section highlighting, updating styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,12 +96,12 @@ const scrollActive = () =>{
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute('id'),
-            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            sectionsClass = document.querySelector('.nav__menu a[href*='+ sectionId + ']')
 
         if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
+            sectionsClass?.classList.add('active-link')
         }else{
-            sectionsClass.classList.remove('active-link')
+            sectionsClass?.classList.remove('active-link')
         }
     })
 }
